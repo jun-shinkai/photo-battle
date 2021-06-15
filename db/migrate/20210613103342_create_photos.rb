688817b2e850f294a77b401a5a7237aa385a67e6
jun-shinkai <shinjyuna@gmail.com>
2021-06-15 18:30:46 +0900

    ユーザー投稿機能

--- db/migrate/20210613103342_create_photos.rb
+++ db/migrate/20210613103342_create_photos.rb
@@ -1,9 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-      t.string                 :photo_name,         null: false, default: ""
-      t.string                 :photo_info,         null: false, default: ""
-      #t.references             :user,               foreign_key:true
+      t.string                 :name,         null: false, default: ""
+      t.string                 :info,         null: false, default: ""
+      t.references             :user,               foreign_key:true
     end
   end
 end
